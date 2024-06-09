cb20b0a610999173312ecf3e796ca27cbd8a582b
ziginfo <34913387+ziginfo@users.noreply.github.com>
2024-06-09 19:52:39 +0200

    Add files via upload

--- live.js
+++ live.js
@@ -1,6 +1,6 @@
 // ========================== VARS ===========================
 var trackcount = local.parameters.numberOfTracks.get() ;
-var scenecount = local.parameters.numberOfClips.get() ;
+var scenecount = local.parameters.numberOfScenes.get() ;
 
 //====================================================================
 //			INITIAL FUNCTIONS 
@@ -8,16 +8,18 @@ var scenecount = local.parameters.numberOfClips.get() ;
 
 function init() {
 
-
+/*
 //	local.send("/live/track/start_listen/volume", "*") ;
 //	local.send("/live/track/start_listen/name", "*") ;
 //	local.send("/live/track/get/output_meter_level", "*") ;
+	local.send("/live/song/start_listen/num_scenes") ;
+	local.send("/live/song/get/num_scenes") ;
 	local.send("/live/song/get/tempo") ;
 	local.send("/live/track/stop_listen/output_meter_left", "*") ;
 	local.send("/live/track/stop_listen/output_meter_right", "*") ;
 //	local.send("/live/track/start_listen/output_meter_left", "*") ;
 //	local.send("/live/track/start_listen/output_meter_right", "*") ;
-
+*/
 
 // =====================================================================
 // 			CREATE CONTAINERS
@@ -33,13 +35,22 @@ Snap = local.values.addStringParameter("Selected Channel", "Shows Selected Chann
 //		cpu.setAttribute("readonly" ,true);
 
 
+//Scenes Labels Container >>>>>>>>>>>>>>>>>>>>>>		
+		scene=local.values.addContainer("Scenes");
+		scene.setCollapsed(true);
+		scene.addTrigger("Sync Scenes", "Get Labels from Live" , false);
+		scene.addTrigger("Reset Scenes", "Reset All Labels" , false);	
+		for (var n = 1; n <= scenecount; n++) {
+			scene.addStringParameter("Scene "+n, "", "");}
+			
 //Track Labels Container >>>>>>>>>>>>>>>>>>>>>>		
 		label=local.values.addContainer("Track Labels");
 		label.setCollapsed(true);
 		label.addTrigger("Sync Labels", "Get Labels from Live" , false);
 		label.addTrigger("Reset Labels", "Reset All Labels" , false);	
 		for (var n = 1; n <= trackcount; n++) {
-			label.addStringParameter("Label "+n, "", ""); }
+			label.addStringParameter("Label "+n, "", "");
+			label.addColorParameter("Color "+n, "Description of my color param",[1,0,1]); }
 			
 // Track Faders Container>>>>>>>>>>>>>>>>>>>>>		
 		faders = local.values.addContainer("Track Volumes");
@@ -92,13 +103,22 @@ function moduleParameterChanged(param) {
 function moduleValueChanged(value) {
   
   	if (value.name == "syncClipInfos"){ 
-  	local.send("/live/track/get/clips/name", "0") ; }
+ // 		local.send("/live/track/start_listen/name", "*") ;
+//  	local.send("/live/clip/get/name", [1,3]) ;
+//  	local.send("/live/track/get/clips/name", "0") ;
+//  	local.send("/live/track/get/arrangement_clips/name", "0") ;
+		local.send("/live/track/get/clips/name", 0) ; 
+  	}
   	
   	if (value.name == "sync"){ 
   	local.send("/live/song/get/tempo") ; }
   	
+  	if (value.name == "syncScenes"){ 
+  	local.send("/live/song/start_listen/name", "*") ;  }
+  	
   	if (value.name == "syncLabels"){ 
-  	local.send("/live/track/start_listen/name", "*") ;}
+  	local.send("/live/track/start_listen/name", "*") ;
+  	local.send("/live/track/start_listen/color", "*") ;}
   	
   	if (value.name == "syncFaders"){ 
   	local.send("/live/track/start_listen/volume", "*") ;  }
@@ -116,7 +136,8 @@ function moduleValueChanged(value) {
   	if (value.name == "resetLabels"){ 
   	for (var n = 0; n < trackcount; n++) {
 	var no = n+1 ;
-	local.values.trackLabels.getChild('Label'+no).set(""); 
+	local.values.trackLabels.getChild('Label'+no).set("");
+	local.values.trackLabels.getChild('Color'+no).set(""); 
 	} }
 		
 	if (value.name == "resetValues"){ 
@@ -136,9 +157,13 @@ function moduleValueChanged(value) {
 
 function oscEvent(address, args) {
 	
- // >>> Tempo
+// >>> Tempo
  	if (address == "/live/song/get/tempo")
- 	{local.values.tempo.set(args[0]);}	
+ 	{local.values.tempo.set(args[0]);}
+ 	
+// >>> Number of Scenes
+ 	if (address == "/live/song/get/num_scenes")
+ 	{local.parameters.numberOfScenes.set(args[0]);}		
 
 
 // >>> insert Track Labels	
@@ -148,6 +173,16 @@ function oscEvent(address, args) {
 	if (address == addr)
 	{if (args[0] == n)
 	 {	local.values.trackLabels.getChild('Label'+no).set(args[1]); } }
+	}
+	
+// >>> insert Track Color	
+	for (var n = 0; n < trackcount; n++) {
+	var no = n+1 ;
+	var addr = "/live/track/get/color" ;
+	if (address == addr)
+	{if (args[0] == n)
+	var col = "1,1,1"  ;
+	 {	local.values.trackLabels.getChild('Color'+no).set([0.5,1,0]); } }
 	}
 
 // >>> insert Fader Volume	
@@ -184,7 +219,6 @@ function update(deltaTime) {
 
  function keepAlive() {
 //	local.send ("/live/track/start_listen/volume", "*");
-//	local.send ("/live/track/get/volume", "*");
 //	local.send("/live/song/get/tempo") ;
 //	local.send("/live/track/start_listen/name", "*") ;
 	}
@@ -298,13 +332,37 @@ function fx_bypass (no, fx, val) {
 	local.send("/track/"+no+"/fx/"+fx+"/bypass", val);
 }
 
+//  =================  Clips and Scenes ===================
+
+function play_scene (no) {
+no= no-1;
+local.send("/live/view/set/selected_scene", no ) ;
+local.send("/live/view/start_listen/selected_scene");
+}
+
+function stop_clips (no) {
+no= no-1;
+local.send("/live/track/stop_all_clips" , no);
+}
+
+function fire_clip (track,clip) {
+track= track-1;
+clip= clip-1;
+local.send("/live/clip/fire", [track , clip]);
+}
+
+function stop_clip (track,clip) {
+track= track-1;
+clip= clip-1;
+local.send("/live/clip/stop", [track , clip]);
+}
+
 //  =================  Song Actions  ===================
 
 function audio_track (no) {
 no= no-1;
 local.send("/live/song/create_audio_track", no);
 }
-
 function midi_track (no) {
 no= no-1;
 local.send("/live/song/create_midi_track", no);
@@ -329,6 +387,10 @@ no= no-1;
 local.send("/live/song/delete_scene", no);
 }
 
+function set_tempo (val) {
+local.send("/live/song/set/tempo", val);
+}
+
 //  =================  Markers  ===================
 
 function prev_mark () {
@@ -338,233 +400,4 @@ local.send("/live/song/jump_to_prev_cue");
 function next_mark () {
 local.send("/live/song/jump_to_next_cue");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function set_snap(val) {
-	val = val-1 ;
-	local.send("/GlobalSnapshots/Recall", val);
-}
-
-function prev_snap() {
-	local.send("/Command/GlobalSnapshots/RecallPreviousGlobalSnapshot");
-}
-
-function next_snap() {
-	local.send("/Command/GlobalSnapshots/RecallNextGlobalSnapshot");
-}
-
-function update_snap() {
-	local.send("/Command/GlobalSnapshots/UpdateActiveGlobalSnapshot");
-}
-
-function add_snap() {
-	local.send("/Command/GlobalSnapshots/AddNewGlobalSnapshot");
-}
-
-/// ===========Cues  ==================
-
-function cuelist_top() {
-	local.send("/Command/CueLists/GoToTop");
-}
-
-function cue_down() {
-	local.send("/Command/CueLists/StepDown");
-}
-
-function cue_up() {
-	local.send("/Command/CueLists/StepUp");
-}
-
-function set_listcue(list, cue) {
-//	val = val-1 ;
-	
-	local.send("/Cue/Recall", [list , cue]);
-}
-
-function set_cue(val) {
-//	val = val-1 ;
-	local.send("/Cue/Recall", val);
-}
-
-function prev_cue() {
-	local.send("/Command/CueLists/FirePreviousCue");
-}
-
-function next_cue() {
-	local.send("/Command/CueLists/FireNextCue");
-}
-
-function stop_cues() {
-	local.send("/Command/CueLists/StopAllCues");
-}
-
-/// =========== Chain Control =============
-
-function add_chain() {
-	local.send("/Command/Chains/AddNewChain");
-}
-
-function prev_chain() {
-	local.send("/Command/PluginWindows/SelectPreviousChain");
-}
-
-function next_chain() {
-	local.send("/Command/PluginWindows/SelectNextChain");
-}
-
-function prev_plug() {
-	local.send("/Command/PluginWindows/SelectPreviousPlugin");
-}
-
-function next_plug() {
-	local.send("/Command/PluginWindows/SelectNextPlugin");
-}
-
-function move_up() {
-	local.send("/GigPerformer/MoveUp");
-}
-
-function move_down() {
-	local.send("/GigPerformer/MoveDown");
-}
-
-/// =========== View Control =============
-
-function recall_view(val) {
-	local.send("/Command/ViewSets/RecallViewSet"+val);
-}
-
-function routing_view() {
-	local.send("/Command/View/PluginAudioRouting");
-}
-
-function wiring_view() {
-	local.send("/Command/ViewModes/WireView");
-}
-
-function chains_view() {
-	local.send("/Command/ViewModes/Chains");
-}
-
-function main_view(val) {
-	local.send("/Command/ViewModes/"+val);
-}
-
-function plug_manager() {
-	local.send("/Command/Options/PluginManager");
-}
-
-function shortcuts() {
-	local.send("/Command/Options/KeyboardShortcuts");
-}
-
-function program_options() {
-	local.send("/Command/Options/ProgramOptions");
-}
-
-function project_options() {
-	local.send("/Command/Options/ProjectOptions");
-}
-
-function fullscreen() {
-	local.send("/Command/View/FullScreen");
-}
-
-/// =========== Show Hide Panels =============
-
-function audio_panel() {
-	local.send("/Command/View/PluginAudioRouting");
-}
-
-function midi_panel() {
-	local.send("/Command/View/PluginMidiPanel");
-}
-
-function plugsnap_panel() {
-	local.send("/Command/View/PluginSnapshotPanel");
-}
-
-function plugpreset_panel() {
-	local.send("/Command/View/PluginPresetList");
-}
-
-function navi_panel() {
-	local.send("/Command/View/Navigator");
-}
-
-function snapshots_panel() {
-	local.send("/Command/View/GlobalSnapshotsPanel");
-}
-
-function cuelists_panel() {
-	local.send("/Command/View/CueListPanel");
-}
-
-function transport_panel() {
-	local.send("/Command/View/TransportPanel");
-}
-
-function workspace_panel() {
-	local.send("/Command/View/WorkspacePanel");
-}
-
-/// =========== Main Actions =============
-
-function save() {
-	local.send("/Command/Project/SaveProject");
-}
-
-function save_as() {
-	local.send("/Command/Project/SaveAs");
-}
-
-function rename_project() {
-	local.send("/Command/Project/RenameProject");
-}
-
-function close() {
-	local.send("/Command/Project/CloseProject");
-}
-
-function open() {
-	local.send("/Command/Project/Open");
-}
-
-function quit() {
-	local.send("/Command/Application/Quit");
-}
-
-function recall_workspace(val) {
-	local.send("/Command/Workspaces/RecallWorkspace"+val);
-}
-
-
 
