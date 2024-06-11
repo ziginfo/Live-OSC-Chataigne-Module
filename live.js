46e2520d99c446b458f569e20ef461a309e1f99d
ziginfo <34913387+ziginfo@users.noreply.github.com>
2024-06-11 20:32:30 +0200

    Add files via upload

--- live.js
+++ live.js
@@ -13,6 +13,80 @@ var trackItems = {
 	"eq" : ["EQ", "b","eq"],
 	"dyn" : ["Dyn", "b","dyn"],
 	"locut" : ["LoCut", "b","loCut"]};
+
+
+var colors = { 
+"1" : [ "32192"     , "0.000" , "0.490 " , "0.753" ]  , 
+"2" : [ "49071"     , "0.000" , "0.749 " , "0.686" ]  , 
+"3" : [ "695438"    , "0.039" , "0.612 " , "0.557" ]  , 
+"4" : [ "1090798"   , "0.063" , "0.643 " , "0.933" ]  , 
+"5" : [ "1698303"   , "0.098" , "0.914 " , "1.000" ]  , 
+"6" : [ "1716118"   , "0.102" , "0.184 " , "0.588" ]  , 
+"7" : [ "1769263"   , "0.102" , "1.000 " , "0.184" ]  , 
+"8" : [ "2319236"   , "0.137" , "0.388 " , "0.518" ]  , 
+"9" : [ "2490280"   , "0.145" , "1.000 " , "0.659" ]  , 
+"10" : [ "3101346"  , "0.184" , "0.322 " , "0.635" ]  , 
+"11" : [ "3947580"  , "0.235" , "0.235 " , "0.235" ]  , 
+"12" : [ "4047616"  , "0.239" , "0.765 " , "0.000" ]  , 
+"13" : [ "5480241"  , "0.325" , "0.624 " , "0.192" ]  , 
+"14" : [ "5538020"  , "0.329" , "0.502 " , "0.894" ]  , 
+"15" : [ "6094824"  , "0.361" , "1.000 " , "0.910" ]  , 
+"16" : [ "6441901"  , "0.384" , "0.294 " , "0.678" ]  , 
+"17" : [ "7491393"  , "0.447" , "0.310 " , "0.255" ]  , 
+"18" : [ "8092539"  , "0.482" , "0.482 " , "0.482" ]  , 
+"19" : [ "8237133"  , "0.490" , "0.690 " , "0.302" ]  , 
+"20" : [ "8623052"  , "0.514" , "0.576 " , "0.800" ]  , 
+"21" : [ "8754719"  , "0.522" , "0.588 " , "0.122" ]  , 
+"22" : [ "8758722"  , "0.522" , "0.647 " , "0.761" ]  , 
+"23" : [ "8912743"  , "0.529" , "1.000 " , "0.404" ]  , 
+"24" : [ "8940772"  , "0.533" , "0.424 " , "0.894" ]  , 
+"25" : [ "8962746"  , "0.533" , "0.761 " , "0.729" ]  , 
+"26" : [ "9160191"  , "0.545" , "0.773 " , "1.000" ]  , 
+"27" : [ "9611263"  , "0.573" , "0.655 " , "1.000" ]  , 
+"28" : [ "10056267" , "0.600" , "0.447 " , "0.294" ]  , 
+"29" : [ "10060650" , "0.600" , "0.514 " , "0.416" ]  , 
+"30" : [ "10204100" , "0.608" , "0.702 " , "0.769" ]  , 
+"31" : [ "10208397" , "0.608" , "0.769 " , "0.553" ]  , 
+"32" : [ "10701741" , "0.639" , "0.294 " , "0.678" ]  , 
+"33" : [ "10851765" , "0.647" , "0.584 " , "0.710" ]  , 
+"34" : [ "10927616" , "0.651" , "0.745 " , "0.000" ]  , 
+"35" : [ "11096369" , "0.663" , "0.318 " , "0.192" ]  , 
+"36" : [ "11119017" , "0.663" , "0.663 " , "0.663" ]  , 
+"37" : [ "11442405" , "0.682" , "0.596 " , "0.898" ]  , 
+"38" : [ "11481907" , "0.686" , "0.200 " , "0.200" ]  , 
+"39" : [ "11958214" , "0.714" , "0.467 " , "0.776" ]  , 
+"40" : [ "12026454" , "0.718" , "0.510 " , "0.337" ]  , 
+"41" : [ "12173795" , "0.725" , "0.757 " , "0.890" ]  , 
+"42" : [ "12243060" , "0.729" , "0.816 " , "0.455" ]  , 
+"43" : [ "12349846" , "0.737" , "0.443 " , "0.588" ]  , 
+"44" : [ "12558270" , "0.749" , "0.624 " , "0.745" ]  , 
+"45" : [ "12565097" , "0.749" , "0.729 " , "0.412" ]  , 
+"46" : [ "12581632" , "0.749" , "0.984 " , "0.000" ]  , 
+"47" : [ "13013643" , "0.776" , "0.573 " , "0.545" ]  , 
+"48" : [ "13381230" , "0.800" , "0.180 " , "0.431" ]  , 
+"49" : [ "13408551" , "0.800" , "0.600 " , "0.153" ]  , 
+"50" : [ "13482980" , "0.804" , "0.733 " , "0.894" ]  , 
+"51" : [ "13496824" , "0.804" , "0.945 " , "0.973" ]  , 
+"52" : [ "13684944" , "0.816" , "0.816 " , "0.816" ]  , 
+"53" : [ "13821080" , "0.824" , "0.894 " , "0.596" ]  , 
+"54" : [ "13872497" , "0.827" , "0.678 " , "0.443" ]  , 
+"55" : [ "13958625" , "0.831" , "0.992 " , "0.882" ]  , 
+"56" : [ "14183652" , "0.847" , "0.424 " , "0.894" ]  , 
+"57" : [ "14402304" , "0.859" , "0.765 " , "0.000" ]  , 
+"58" : [ "14837594" , "0.886" , "0.404 " , "0.353" ]  , 
+"59" : [ "15029152" , "0.898" , "0.325 " , "0.627" ]  , 
+"60" : [ "15064289" , "0.898" , "0.863 " , "0.882" ]  , 
+"61" : [ "15597486" , "0.929" , "1.000 " , "0.682" ]  , 
+"62" : [ "16149507" , "0.965" , "0.424 " , "0.012" ]  , 
+"63" : [ "16249980" , "0.969" , "0.957 " , "0.486" ]  , 
+"64" : [ "16725558" , "1.000" , "0.212 " , "0.212" ]  , 
+"65" : [ "16726484" , "1.000" , "0.224 " , "0.831" ]  , 
+"66" : [ "16749734" , "1.000" , "0.580 " , "0.651" ]  , 
+"67" : [ "16753524" , "1.000" , "0.639 " , "0.455" ]  , 
+"68" : [ "16753961" , "1.000" , "0.647 " , "0.161" ]  , 
+"69" : [ "16773172" , "1.000" , "0.941 " , "0.204" ]  , 
+"70" : [ "16777215" , "1.000" , "1.000 " , "1.000" ] };
+
 //====================================================================
 //			INITIAL FUNCTIONS 
 //====================================================================
@@ -32,8 +106,8 @@ function init() {
 	tempo = local.values.addFloatParameter("Tempo", "Shows the Temp en BPM", 120, 30, 400);
 	tempo.setAttribute("ui", "label") ;
 	setTempo = local.values.addTrigger("Set New Tempo" , "Set Tempo" , false);
-	metersOn = local.values.addTrigger("Activate Meters" , "Request values from Live !!" , false);
-	metersOff = local.values.addTrigger("Stop Meters" , "Stop All Feedback" , false);
+//	metersOn = local.values.addTrigger("Activate Meters" , "Request values from Live !!" , false);
+//	metersOff = local.values.addTrigger("Stop Meters" , "Stop All Feedback" , false);
 	songtimeOff = local.values.addTrigger("Stop Songtime" , "Stop Songtime Feedback" , false);
 	
 // Track Items Container >>>>>>>>>>>>>>>>>>>>>>
@@ -101,6 +175,8 @@ function init() {
 // Channel Meters Container>>>>>>>>>>>>>>>>>>>>>		
 		meter = local.values.addContainer("Meters");
 		meter.setCollapsed(true);
+		meter.addTrigger("Activate Meters" , "Request values from Live !!" , false);
+		meter.addTrigger("Stop Meters" , "Stop All Feedback" , false);
 		for (var n = 1; n <= trackcount; n++) {
 			var met = meter.addFloatParameter("Track "+n, "", 0, 0, 1);
 			met.setAttribute("readonly" ,true);  
@@ -158,7 +234,7 @@ function moduleValueChanged(value) {
   		
 // >>>>> Stop All Feedback 	
  	if (value.name == "stopAllFeedback"){ 
-  		local.send("/live/song/stop_listen/current_song_time") ;  
+  		local.send("/live/song/stop_listen/beat") ;  
 		local.send("/live/song/stop_listen/*");
 		local.send("/live/track/stop_listen/*", "*");
 	}
@@ -181,7 +257,7 @@ function moduleValueChanged(value) {
   		local.send("/live/track/start_listen/arm", "*") ;
   		local.send("/live/view/start_listen/selected_track") ;
   		local.send("/live/view/start_listen/selected_scene") ;
-		local.send("/live/song/start_listen/current_song_time");
+		local.send("/live/song/start_listen/beat");
 		for (var n = 0; n < trackcount; n++) {
 		local.send("/live/track/get/is_grouped", n); } 
   	}
@@ -191,7 +267,7 @@ function moduleValueChanged(value) {
   		local.send("/live/song/set/tempo", temp) ; }
 // >>>>> Stop Songtime  	
   	if (value.name == "stopSongtime"){ 
-  		local.send("/live/song/stop_listen/current_song_time") ;  
+  		local.send("/live/song/stop_listen/beat") ;  
 		local.values.infos.songTimeMeasure.set(1);
 		local.values.infos.songTimeBeats.set(1);
 	}
@@ -245,6 +321,7 @@ function moduleValueChanged(value) {
 		var no = n+1 ;
 		var child = "Track"+no ;
 		local.values.tracks.getChild(child).label.set("");
+		local.values.tracks.getChild(child).color.set(0,0,0);
 		local.values.tracks.getChild(child).meter.set(0);
 		local.values.tracks.getChild(child).fader.set(0);
 		local.values.tracks.getChild(child).pan.set(0);
@@ -273,7 +350,7 @@ function oscEvent(address, args) {
  		local.values.selectedTrack.set(args[1]);}
 
 // >>> Song Time 	
- 	if (address == "/live/song/get/current_song_time") {
+ 	if (address == "/live/song/get/beat") {
  		var args = args[0] +1 ;
 		var meas = (args +3) / 4 ;
 		var compte = Math.round(meas * 4) ;
@@ -289,18 +366,27 @@ function oscEvent(address, args) {
  		local.values.infos.allTracks.set(args[0]);}	
 
 
-//  >>>>>>>> INSERT TRACK VALUES <<<<<<<<<<<<<<<<	
-/*
+//  >>>>>>>> INSERT TRACK VALUES <<<<<<<<<<<<	
+
 // >>> insert Track Color	
 	for (var n = 0; n < trackcount; n++) {
 	var no = n+1 ;
 	var addr = "/live/track/get/color" ;
-	if (address == addr)
-	{if (args[0] == n)
-	var col = "1,1,1"  ;
-	 {	local.values.trackLabels.getChild('Color'+no).set([0.5,1,0]); } }
-	}
-*/
+	if (address == addr) {
+	local.values.tracks.test1.set(0.5,0.5,0.5);
+	local.values.tracks.test2.set(0.599,0.599,0.599);
+	if (args[0] == n){
+	var col = args[1] ;	
+	var items = util.getObjectProperties(colors);
+	var val3 = colors[items[3]][0] ;
+	for (var c = 0; c < items.length; c++) {
+	if (colors[items[c]][0] == col) {
+	var val1 = colors[items[c]][1] ;
+	var val2 = colors[items[c]][2] ;
+	var val3 = colors[items[c]][3] ;
+	local.values.tracks.getChild('Track'+no).color.set(val1,val2,val3);
+	}  } } } }
+
 // >>> insert Track Labels	
 	for (var n = 0; n < scenecount; n++) {
 	var no = n+1 ;
@@ -388,14 +474,12 @@ function oscEvent(address, args) {
 	var mo = m + 1 ;
 	local.values.clips.getChild('Track'+no+'Clips').getChild('clip'+mo).set(args[2]);} } 	
 	}  }  }		
-	
-	
+		
 }
 
 //========================================================================
 //					KEEP ALIVE
 //========================================================================
-
 
 function update(deltaTime) {
 	var now = util.getTime();
@@ -411,7 +495,9 @@ function update(deltaTime) {
 // 			GENERIC FUNCTIONS
 // =====================================================================
 
-// Generic Functions
+// ____________________________________________
+// >>>>>> Generic Functions <<<<<<<<<<<<<<<
+// ____________________________________________
 
 //===========TRACKS ================
 function master_volume(val) {
@@ -478,6 +564,9 @@ function taptempo() {
 	local.send("/live/song/tap_tempo");
 }
 
+function metronome(val) {
+	local.send("/live/song/set/metronome", val);
+}
 function bank_back() {
 	local.send("/device/track/bank/-");
 }
